79b3359ebd23a28553697762aaa8454d498e01e4
santa-sheesh <134328127+santa-sheesh@users.noreply.github.com>
2023-05-23 15:22:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,68 @@
-//c
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+/*#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>*/
+/*u tekstualnu datoteku unesi kut kretanja i prema tome nastavi kod*/
+int main() {
+
+	FILE* fps;
+	fps = fopen("dvdlogo.jpg", "r+");
+
+	if (!fps) {
+        printf("Greska pri otvaranju slike!");
+		
+		exit(1);
+	}
+
+	FILE* fp = fopen("test.txt", "w");
+	if (!fp) {
+		printf("Greska pri otvaranju datoteke!");
+		exit(1);
+	}
+	
+	int yourChoice = 0;
+
+	printf("Izaberi pocetni kut kretanja loga: \n1 - (gornji desni) \n2 - (gornji lijevi) \n3 - (donji desni) \n4 - (donji lijevi)\n");
+	printf("Tvoj odabir: ");
+	scanf_s("%d", &yourChoice);
+
+	switch (yourChoice) {
+	case 1:
+		fprintf(fp, "Kut je gornji desni\n");
+		fclose(fp);
+		rename("test.txt", "gd.txt");
+		printf("Done!");
+		break;
+
+	case 2:
+		fprintf(fp, "Kut je gornji lijevi\n");
+		fclose(fp);
+		rename("test.txt", "gl.txt");
+		printf("Done!");
+		break;
+
+	case 3:
+		fprintf(fp, "Kut je donji desni\n");
+		fclose(fp);
+		rename("test.txt", "dd.txt");
+		printf("Done!");
+		break;
+
+	case 4:
+		fprintf(fp, "Kut je donji lijevi\n");
+		fclose(fp);
+		rename("test.txt", "dl.txt");
+		printf("Done!");
+		break;
+
+	default:
+		break;
+	}
+
+	
+	fclose(fp);
+
+	
+	return 0;
+}
