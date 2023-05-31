98721b8eb7f4b19180776d720e58be0069e2fc7b
santa-sheesh <134328127+santa-sheesh@users.noreply.github.com>
2023-06-01 00:00:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,68 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-/*#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>*/
-/*u tekstualnu datoteku unesi kut kretanja i prema tome nastavi kod*/
-int main() {
 
-	FILE* fps;
-	fps = fopen("dvdlogo.jpg", "r+");
+#define MAX_LINE_LENGTH 100
+#define TERMINAL_LINES 25
 
-	if (!fps) {
-        printf("Greska pri otvaranju slike!");
-		
-		exit(1);
-	}
+void printArt (FILE* file, int corner){
+  char line[MAX_LINE_LENGTH];
+  int lineNumber = 0;
 
-	FILE* fp = fopen("test.txt", "w");
-	if (!fp) {
-		printf("Greska pri otvaranju datoteke!");
-		exit(1);
-	}
-	
-	int yourChoice = 0;
+  while (fgets (line, MAX_LINE_LENGTH, file) != NULL){
+      lineNumber++;
+    }
 
-	printf("Izaberi pocetni kut kretanja loga: \n1 - (gornji desni) \n2 - (gornji lijevi) \n3 - (donji desni) \n4 - (donji lijevi)\n");
-	printf("Tvoj odabir: ");
-	scanf_s("%d", &yourChoice);
+  fseek (file, 0, SEEK_SET);
 
-	switch (yourChoice) {
-	case 1:
-		fprintf(fp, "Kut je gornji desni\n");
-		fclose(fp);
-		rename("test.txt", "gd.txt");
-		printf("Done!");
-		break;
+  int linesToSkip = 0;
+  switch (corner){
+    case 1:			
+      linesToSkip = 0;
+      break;
+    case 2:			
+      linesToSkip = TERMINAL_LINES - lineNumber;
+      break;
+    default:
+      printf ("PogreE!na opcija za kut.\n");
+      return;
+    }
 
-	case 2:
-		fprintf(fp, "Kut je gornji lijevi\n");
-		fclose(fp);
-		rename("test.txt", "gl.txt");
-		printf("Done!");
-		break;
+  for (int i = 0; i < linesToSkip; i++){
+      printf ("\n");
+    }
 
-	case 3:
-		fprintf(fp, "Kut je donji desni\n");
-		fclose(fp);
-		rename("test.txt", "dd.txt");
-		printf("Done!");
-		break;
+  while (fgets (line, MAX_LINE_LENGTH, file) != NULL){
+      printf ("%s", line);
+    }
+}
 
-	case 4:
-		fprintf(fp, "Kut je donji lijevi\n");
-		fclose(fp);
-		rename("test.txt", "dl.txt");
-		printf("Done!");
-		break;
+int main (){
+  FILE *file = fopen ("file.txt", "r");
+  if (!file){
+      printf ("GreE!ka pri otvaranju datoteke.\n");
+      return 1;
+    }
 
-	default:
-		break;
-	}
+  int corner;
+  printf ("Odaberite kut za ASCII art (1-Gornji, 2-Donji): ");
+  scanf ("%d", &corner);
 
-	
-	fclose(fp);
+  printArt (file, corner);
 
-	
-	return 0;
+  fclose (file);
+  return 0;
 }
