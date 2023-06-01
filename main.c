a5c62ebcc4f4098b42908fb2942c106e48bbe8cd
santa-sheesh <134328127+santa-sheesh@users.noreply.github.com>
2023-06-01 09:54:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include "header.h"
 
 int main() {
 	FILE* file = fopen("file.txt", "r");
 	if (!file) {
-		perror("Greska pri otvaranju datoteke!\n");
+		perror("\n");
 		return 1;
 	}
 
 	int corner = 0;
 	printf("Odaberite kut za ASCII art (1-Gornji, 2-Donji): ");
-	scanf("%d", &corner);
+	
+	do {
+	scanf_s("%d", &corner);
+	} while (corner < 1 || corner > 2);
 
-	printArt(file, corner);
+	int linesToSkip = 0;
+	char key;
+	
+	while ((key=_getch())!='*') {
+		system("cls");
 
+		if (key == 'w' || key == 'W') {
+			if (linesToSkip > 0) {
+				linesToSkip--;
+			}
+		}
+		else if (key == 's' || key == 'S') {
+			linesToSkip++;
+		}
+		printf("(W/w za gore)\n(S/s za dolje)\n(Pritisni '*' za prekid programa)\n");
+		printArt(file, corner, linesToSkip);
+	}
+    
 	fclose(file);
 	return 0;
 }
-
