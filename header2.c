06e5ee8654504043a57e7edf27e8c21badf6dbfa
santa-sheesh <134328127+santa-sheesh@users.noreply.github.com>
2023-06-06 09:54:11 +0200

    Update header2.c

--- header2.c
+++ header2.c
@@ -1,1 +1,12 @@
+#pragma once
+typedef struct {
+	char** lines;
+	int count;
+} AsciiArt;
 
+#define MAX_LINE_LENGTH 100
+#define TERMINAL_LINES 40
+
+void printArt(const AsciiArt* art, int linesToSkip, int corner);
+void freeAsciiArt(AsciiArt* art);
+void repetition(FILE* fp);
